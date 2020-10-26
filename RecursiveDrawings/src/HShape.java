61539db4095b7a5e6a569e22114ca7b10e0d4286
Tram Nguyen <72321705+tramnguyen1127@users.noreply.github.com>
2020-10-26 12:01:47 -0700

    Update HShape.java

--- RecursiveDrawings/src/HShape.java
+++ RecursiveDrawings/src/HShape.java
@@ -1,4 +1,15 @@
 
-public class HShape extends AbstractShape{
+public class HShape extends AbstractShape {
+  
+  public HShape() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void draw(Graphics g) {
+		
+		
+	}
+
 
 }
