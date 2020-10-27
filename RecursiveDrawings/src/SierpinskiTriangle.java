fbe00cfc64548541219b9781e04d0eac0dff0bc5
Tram Nguyen <72321705+tramnguyen1127@users.noreply.github.com>
2020-10-26 20:48:42 -0700

    Update SierpinskiTriangle.java

--- RecursiveDrawings/src/SierpinskiTriangle.java
+++ RecursiveDrawings/src/SierpinskiTriangle.java
@@ -1,14 +1,30 @@
 
 public class SierpinskiTriangle extends AbstractShape {
-  
-  public SierpinskiTriangle () {
+ 
+	Polygon sierpinskiTriangle;
+	private int dimension = 800;
+	private int height;
+   
+    public SierpinskiTriangle() {
+
+    	sierpinskiTriangle = new Polygon();
+
+        height = (int) Math.round(dimension / 1.4);
+        
+        sierpinskiTriangle.addPoint(20, height);
+        sierpinskiTriangle.addPoint(dimension / 2, 0);
+        sierpinskiTriangle.addPoint(dimension - 20, height);
+        
+
+    }
 		
 		// TODO Auto-generated constructor stub
 	}
 	
 	public void draw(Graphics g) {
 		
-		
+		g.setColor(Color.green);
+		g.drawPolygon(sierpinskiTriangle);
 	}
 
 
