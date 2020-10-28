ff50bffd77b4dd38ebf06af03c18446e7883843b
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-10-28 10:12:26 -0600

    Updated HShape
    -Added maxlevel and level variables
    -Added call to super constructor
    -Added method createChildren
    -Added update method (currently empty)

--- RecursiveDrawings/src/HShape.java
+++ RecursiveDrawings/src/HShape.java
@@ -1,15 +1,36 @@
+import java.awt.Graphics;
 
 public class HShape extends AbstractShape {
-  
-  public HShape() {
-		
+
+	private static int maxLevel = 5;
+	private static int level = 7;
+
+	public HShape() {
+		super(maxLevel, level);
 		// TODO Auto-generated constructor stub
+		// H's at level n are children of H's at level n-1
+		// each level adds 7 H's.
+	}
+
+	public void draw(Graphics g) {
+
+	}
+
+	public void createChildren() {
+		//fill the array of children 
+		for(int i = 0; i<children.length; i++) {
+			children[i] = new HShape();
+		}
 	}
 	
-	public void draw(Graphics g) {
-		
-		
+	/**
+	 * EXTRA CREDIT
+	 * Modifies this shape in an interesting way given a value in the range 0-100.
+	 * Initially the slider should be set to 50.
+	 * 
+	 * @param int value - range between 0-100
+	 */
+	public void update(int value) {
+
 	}
-
-
 }
