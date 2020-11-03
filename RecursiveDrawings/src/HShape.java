d694ed7204b21e15296092913a06638eda9109b0
Tram Nguyen <72321705+tramnguyen1127@users.noreply.github.com>
2020-11-03 13:22:46 -0800

    HShape is completed

--- RecursiveDrawings/src/HShape.java
+++ RecursiveDrawings/src/HShape.java
@@ -1,11 +1,16 @@
 import java.awt.Graphics;
 
 public class HShape extends AbstractShape {
-	
-	Polygon HShape;
-	private Point r1, r2, r3, r4, r5, r6, r7, p_hw;
+
 	private static int maxLevel = 5;
-	private static int level = 7;
+	
+	private static int length = 7;
+	
+	private int h, w;
+	
+	private Point r1_start, r2_start, r3_start, r4_start, r5_start, r6_start, r7_start, p_hw;
+	
+	private Point r1_end, r2_end, r3_end, r4_end, r5_end, r6_end, r7_end;
 
 	public HShape(int height, int width) {
 		super(maxLevel, length);
@@ -15,49 +20,62 @@ public class HShape extends AbstractShape {
 		
 		p_hw = new Point(w / 3, h / 3);
 		
-		// first rectangle
-		r1 = new Point(w - w, h - h);
-		// second rectangle
-		r2 = new Point(w - w, h / 3);
-		// third rectangle
-		r3 = new Point(w - w, (h / 3) * 2);
-		// fourth rectangle
-		r4 = new Point(w / 3, h / 3);
-		// fifth rectangle
-		r5 = new Point((w / 3) * 2, h - h);
-		// sixth rectangle
-		r6 = new Point((w / 3) * 2, h / 3);
-		// seventh rectangle
-		r7 = new Point((w / 3) * 2, (h / 3) * 2);
+		// The start points of the seven rectangles
+		r1_start = new Point(w - w, h - h);
+		r2_start = new Point(w - w, h / 3);
+		r3_start = new Point(w - w, (h / 3) * 2);
+		r4_start = new Point(w / 3, h / 3);
+		r5_start = new Point((w / 3) * 2, h - h);
+		r6_start = new Point((w / 3) * 2, h / 3);
+		r7_start = new Point((w / 3) * 2, (h / 3) * 2);
+		
+		// The end points of the seven rectangles
+		r1_end = new Point(w / 3, h / 3);
+		r2_end = new Point(w / 3, (h / 3) * 2);
+		r3_end = new Point(w / 3, h);
+		r4_end = new Point((w / 3) * 2, (h / 3) * 2);
+		r5_end = new Point(w, h / 3);
+		r6_end = new Point(w, (h / 3) * 2);
+		r7_end = new Point(w, h);
 		
 	}
 	
-	public HShape(Point p_1, Point p_2) {
+	public HShape(Point p1, Point p2) {
 		super(maxLevel, length);
 		
-		w = p_2.x - p_1.x;
-		h = p_2.y - p_1.y;
+		w = p2.x - p1.x;
+		h = p2.y - p1.y;
 		
-		r1 = new Point(p_1.x, p_1.y);
+		p_hw = new Point(w / 3, h / 3);
 		
+		// The start points of the seven rectangles (based on the two given points)
+		r1_start = new Point(p1.x, p1.y);
+		r2_start = new Point(p1.x, p1.y + ((p2.y - p1.y) / 3));
+		r3_start = new Point(p1.x, p1.y + (((p2.y - p1.y) / 3)) * 2);
+		r4_start = new Point(p1.x + (p2.x - p1.x) / 3, p1.y + ((p2.y - p1.y) / 3));
+		r5_start = new Point(p1.x + ((p2.x - p1.x) / 3) * 2, p1.y);
+		r6_start = new Point(p1.x + ((p2.x - p1.x) / 3) * 2, p1.y + ((p2.y - p1.y) / 3));
+		r7_start = new Point(p1.x + ((p2.x - p1.x) / 3) * 2, p1.y + (((p2.y - p1.y) / 3)) * 2);
+		
+		// The end points of the seven rectangles (based on the two given points)
+		r1_end = new Point(p1.x + (p2.x - p1.x) / 3, p1.y + ((p2.y - p1.y) / 3));
+		r2_end = new Point(p1.x + (p2.x - p1.x) / 3, p1.y + (((p2.y - p1.y) / 3)) * 2);
+		r3_end = new Point(p1.x + (p2.x - p1.x) / 3, p2.y);
+		r4_end = new Point(p1.x + ((p2.x - p1.x) / 3) * 2, p1.y + (((p2.y - p1.y) / 3)) * 2);
+		r5_end = new Point(p2.x, p1.y + (p2.y - p1.y) / 3);
+		r6_end = new Point(p2.x, p1.y + ((p2.y - p1.y) / 3) * 2);
+		r7_end = new Point(p2.x, p2.y);	
 	}
 	
 	public void createChildren() {
     	
-		children[0] = new HShape(r1, r4);
-		children[1] = new HShape(r2, new Point(w / 3, h));
-		children[2] = new HShape(r1, r4);
-		children[3] = new HShape(r1, r4);
-		children[4] = new HShape(r1, r4);
-		children[5] = new HShape(r1, r4);
-		children[6] = new HShape(r1, r4);
-//		children[1] = new HShape(r2, new Point(w / 3, (h / 3) * 2));
-//		children[2] = new HShape(r3, new Point(w / 3, h));
-//		children[3] = new HShape(r4, r7);
-//		children[4] = new HShape(r5, new Point(w, h / 3));
-//		children[5] = new HShape(r6, new Point(w, (h / 3) * 2));
-//		children[6] = new HShape(r7, new Point(w, h));
-    	
+		children[0] = new HShape(r1_start, r1_end);
+		children[1] = new HShape(r2_start, r2_end);
+		children[2] = new HShape(r3_start, r3_end);
+		children[3] = new HShape(r4_start, r4_end);
+		children[4] = new HShape(r5_start, r5_end);
+		children[5] = new HShape(r6_start, r6_end);
+		children[6] = new HShape(r7_start, r7_end);	
     }
     
 	
