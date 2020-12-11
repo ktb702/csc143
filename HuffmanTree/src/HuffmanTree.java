fd025148546358768cb5692e399513515dc9adbd
Tram Nguyen <72321705+tramnguyen1127@users.noreply.github.com>
2020-12-10 17:36:04 -0800

    COMPLETE

--- HuffmanTree/src/HuffmanTree.java
+++ HuffmanTree/src/HuffmanTree.java
@@ -1,5 +1,4 @@
 import java.io.PrintStream;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -19,63 +18,35 @@ public class HuffmanTree {
 		
 		PriorityQueue <HuffmanNode> pQ = new PriorityQueue <HuffmanNode> ();
 		
-		for (int i = 0; i < count.length; i++) {
+		/**
+		*Loop through the count array and put the ascii values and frequencies into the queue
+		*/
+		for (int i = 1; i < count.length; i++) {
 			if (count[i] > 0) {
 				HuffmanNode node = new HuffmanNode(i, count[i]);
 				pQ.offer(node);
 			}
 		}
 		
-		//write code to create a fictitious end of file character. 
-		//Value will be one higher than the highest value of chars in the frequency array passed to the constructor. 
-		//manually add this to the priority queue, and will have a frequency of 1.
+		/** Fictitious end of file character. 
+		 * Value will be one higher than the highest value of chars in the frequency array passed to the constructor. 
+		 * Manually add this to the priority queue, and will have a frequency of 1.
+		 */
 		HuffmanNode endOfFile = new HuffmanNode(count.length, 1);
 		pQ.offer(endOfFile);
 		
+		/**
+		 * Loop through the queue to combine nodes and build the tree
+		 */
 		while (pQ.size() >= 2) {
 			HuffmanNode node1 = pQ.poll();
 			HuffmanNode node2 = pQ.poll();
 			pQ.offer( new HuffmanNode(-1, node1.frequency + node2.frequency, node1, node2));
 		}
-		overallRoot = pQ.poll();
+		// Set overallRoot to the top of the tree
+		overallRoot = pQ.poll();	
 	}
 	
-	
-	
-	// inner class to describe a Huffman Node
-	private class HuffmanNode implements Comparable <HuffmanNode> {
-		public int frequency;
-		public int ascii;
-		public HuffmanNode left, right;
-		
-		public HuffmanNode(int ascii, int frequency) { // this doesnt affect the output
-			this(ascii, frequency, null, null);
-		}
-		
-		public HuffmanNode(int ascii, int frequency, HuffmanNode n1, HuffmanNode n2) {
-			this.ascii = ascii;
-			this.frequency = frequency;
-			this.left = n1;
-			this.right = n2;
-		}
-		
-		/**
-		 * Returns < 0 if the frequency of this node is < frequency of h
-		 * Returns 0 if the frequency of this node is == frequency of h
-		 * Returns > 0 if the frequency of this node is > frequency of h
-		 */
-		public int compareTo(HuffmanNode h) {
-			return this.frequency - h.frequency;
-		}
-		
-		public boolean hasLeft() {
-			return this.left != null;
-		}
-		
-		public boolean hasRight() {
-			return this.right != null;
-		}
-	}
 	
 	/**
 	 * Writes the current tree to the given output stream in standard format.
@@ -84,7 +55,6 @@ public class HuffmanTree {
 	public void write(PrintStream output) {
 		write(output, overallRoot, "");
 	}
-	
 	/**
 	 * A helper function for recursion
 	 * 
@@ -105,6 +75,7 @@ public class HuffmanTree {
 		}
 	}
 	
+	
 	/**
 	 * Constructs a Huffman tree from the Scanner.  Assumes the Scanner contains a tree description in standard format.
 	 * 
@@ -113,14 +84,12 @@ public class HuffmanTree {
 	public HuffmanTree(Scanner input) {
 		while (input.hasNextLine()) {
 			// scan for the input ascii value and code
-			int ascii = input.nextInt();
-			input.nextLine();
+			int ascii = Integer.parseInt(input.nextLine());
 			String code = input.nextLine();
-			
-			treeBuilding(overallRoot, ascii, code);
+			// create the tree and set to overallRoot
+			overallRoot = treeBuilding(overallRoot, ascii, code); 
 		}
 	}
-	
 	/**
 	 * A helper function to create the Huffman tree from the Scanner
 	 * 
@@ -130,19 +99,24 @@ public class HuffmanTree {
 	 * @return root
 	 */
 	private HuffmanNode treeBuilding(HuffmanNode root, int ascii, String code) {
+		// if there is nothing in the tree yet, build the root
 		if (root == null) {
 			root = new HuffmanNode(-1, 0);
-		} else if (code.length() == 1) {
+		} 
+		// base case: create the left and right children of the tree
+		if (code.length() == 1) {
 			if (code.equals("0")) {
 				root.left = new HuffmanNode(ascii, 0);
 			} else {
 				root.right = new HuffmanNode(ascii, 0);
 			}
-		} else {
+		} 
+		// recursive step: examine the code and traverse down the tree
+		else {
 			if (code.charAt(0) == '0') {
-				treeBuilding(root.left, ascii, code.substring(1));
+				root.left = treeBuilding(root.left, ascii, code.substring(1));
 			} else {
-				treeBuilding(root.right, ascii, code.substring(1));
+				root.right = treeBuilding(root.right, ascii, code.substring(1));
 			}
 		}
 		return root;
@@ -159,25 +133,83 @@ public class HuffmanTree {
 	 * @param output
 	 * @param charMax
 	 */
-	public void decode(BitInputStream input, PrintStream output, int charMax) {
-		int bit = input.readBit();
+	public void decode (BitInputStream input, PrintStream output, int charMax) {
+		int ascii = readBit(input); // read the bits from the input for the ascii value 
+		// only proceed if the ascii value does not exceed the value of charMax
+		// (not at the end of the file)
+		while (ascii < charMax) {
+			output.write(ascii); // write the ascii value to the output file
+			ascii = readBit(input); // read the next ascii value
+		}
+	}
+	/**
+	 * Reads the bits from the input file until it reaches the leaf node
+	 * 
+	 * @param input  
+	 * @return ascii value of the leaf
+	 */
+	private int readBit(BitInputStream input) {
 		HuffmanNode node = overallRoot;
-
-		while (bit != -1) { //readBit returns -1 if at eof
-
-			if (node.hasRight() == false && node.hasLeft() == false && node.ascii == eof) {
-				bit = -1; //end the loop we are at eof
-			} else {
-				if (bit == 0) {
-					node = node.left;
-				} else {
-					node = node.right;
-				}
-				output.write(node.ascii);
-				bit = input.readBit();
+		
+		// while the leaf has not been reached, keep traversing the tree to the left or right
+		// depends on the bits read
+		while (node.ascii < 0) {
+			int bit = input.readBit(); // read the bit
+			// traverse to the left if bit == 0
+			if (bit == 0) {
+				node = node.left;
+			} 
+			// traverse to the right if bit == 1
+			else {
+				node = node.right;
 			}
 		}
-		
+		// return the ascii value of the leaf (no longer < 0 (-1))
+		return node.ascii;
 	}
+	   
 	
+	   
+	/**
+	 * The inner HuffmanNode class
+	 * 
+	 * @author CSC 143
+	 */
+	private class HuffmanNode implements Comparable <HuffmanNode> {
+		public int frequency; // the counts of this character
+		public int ascii; // the ascii value of this character
+		public HuffmanNode left, right; // left and right children of the tree
+			
+		// constructors
+		public HuffmanNode(int ascii, int frequency) {
+			this(ascii, frequency, null, null);
+		}	
+		public HuffmanNode(int ascii, int frequency, HuffmanNode n1, HuffmanNode n2) {
+			this.ascii = ascii;
+			this.frequency = frequency;
+			this.left = n1;
+			this.right = n2;
+		}
+		
+		/**
+		 * Returns < 0 if the frequency of this node is < frequency of h
+		 * Returns 0 if the frequency of this node is == frequency of h
+		 * Returns > 0 if the frequency of this node is > frequency of h
+		 */
+		public int compareTo(HuffmanNode h) {
+			return this.frequency - h.frequency;
+		}	
+		/**
+		 * @return true if this node has a left children
+		 */
+		public boolean hasLeft() {
+			return this.left != null;
+		}
+		/**	
+		 * @return true if this node has a right children
+		 */
+		public boolean hasRight() {
+			return this.right != null;
+		}
+	}	
 }
